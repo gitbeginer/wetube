c3f6a8ee2e2ddbab89fd563915a9aec42db014c3
echo <echo@echo.com>
2021-09-14 05:48:24 +0900

    cccommit

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import express from "express"
 const app = express();
 const port = 8088;
 app.listen(port,()=>console.log(`Listening http://localhost:${port}`))
-app.get("/",(rq,rp)=>{
-    console.log(rq)
-})
+
+const middle = (rq,rs,next)=>{
+    console.log("this is middle " + rq.url);
+    next();
+}
+function endrs(rq,rs){
+    return rs.send("HEY YOU");
+}
+app.use(middle);
+app.get("/", endrs); 
