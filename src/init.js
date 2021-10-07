6dc2263a2ec49042fe7d5df261d482d50ef78b9e
echo <echo@echo.com>
2021-10-08 03:32:28 +0900

    dell

--- src/init.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config"
-import "./db"
-// import "./models/Video"
-// import "./models/User";
-import app from "./server.js"
-const port = 8088;
-
-
-app.listen(port,()=>console.log(`Listening http://localhost:${port}`))
