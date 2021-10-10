a867a3a91978458b96c0e4e0f195e650cdac7025
echo <echo@echo.com>
2021-10-10 15:24:54 +0900

    ddd

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -209,7 +209,7 @@ export const see = async (req, res) => {
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
-  const videos = await Video.find({ owner: user._id });
+  const videos = await Video.find({ owner: user._id }).populate("owner");
   return res.render("users/profile", {
     pageTitle: user.name,
     user,
