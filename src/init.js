4cf36db730032eca0ad096b2d46e59625ecb120d
echo <echo@echo.com>
2021-10-13 22:08:52 +0900

    cam record

--- src/init.js
+++ src/init.js
@@ -3,7 +3,7 @@ import "./db"
 // import "./models/Video"
 // import "./models/User";
 import app from "./server.js"
-const port = 8088;
+const port = 80;
 
 
 app.listen(port,()=>console.log(`Listening http://localhost:${port}`))
